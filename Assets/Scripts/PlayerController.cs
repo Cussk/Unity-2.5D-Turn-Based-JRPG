1b3d44db6dba93c6ce2f83db7006a759cf116c4e
Kyle Cuss <108804713+Cussk@users.noreply.github.com>
2024-03-25 08:17:01 -0230

    Setup scenes and encounter transition logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,33 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 
 public class PlayerController : MonoBehaviour
 {
     const string IS_WALKING_PARAM = "IsWalking";
+    const string BATTLE_SCENE = "BattleScene";
+    const float TIME_PER_STEP = 0.5F;
     
     [SerializeField] Animator animator;
     [SerializeField] SpriteRenderer playerSprite;
+    [SerializeField] LayerMask grassLayer;
+    [SerializeField] int stepsInGrass;
     [SerializeField] int speed;
+    [SerializeField] int minStepsToEncounter;
+    [SerializeField] int maxStepsToEncounter;
+    
     PlayerControls _playerControls;
     Rigidbody _rigidbody;
     Vector3 _movement;
+    bool _movingInGrass;
+    float _stepTimer;
+    int _stepsToEncounter;
     
     void Awake()
     {
         _playerControls = new PlayerControls();
-        
+        SetRandomStepsToEncounter();
     }
 
     void OnEnable()
@@ -35,9 +47,12 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-       _rigidbody.MovePosition(transform.position + _movement * (speed * Time.fixedDeltaTime)); 
+        var position = transform.position;
+        _rigidbody.MovePosition(position + _movement * (speed * Time.fixedDeltaTime));
+
+       AddStepsUntilEncounter(position);
     }
-    
+
     void PlayerMove()
     {
         var x = _playerControls.Player.Move.ReadValue<Vector2>().x;
@@ -62,5 +77,33 @@ public class PlayerController : MonoBehaviour
                 playerSprite.flipX = false;
                 break;
         }
+    }
+    
+    void AddStepsUntilEncounter(Vector3 position)
+    {
+        var colliders = Physics.OverlapSphere(position, 1, grassLayer);
+        _movingInGrass = colliders.Length != 0 && _movement != Vector3.zero;
+
+        if (!_movingInGrass) return;
+        _stepTimer += Time.fixedDeltaTime;
+        
+        if (_stepTimer > TIME_PER_STEP)
+        {
+            stepsInGrass++;
+            _stepTimer = 0;
+
+            if (stepsInGrass >= _stepsToEncounter)
+            {
+                SceneManager.LoadScene(BATTLE_SCENE);
+                
+                stepsInGrass = 0;
+                SetRandomStepsToEncounter();
+            }
+        }
+    }
+
+    void SetRandomStepsToEncounter()
+    {
+        _stepsToEncounter = Random.Range(minStepsToEncounter, maxStepsToEncounter + 1);
     }
 }

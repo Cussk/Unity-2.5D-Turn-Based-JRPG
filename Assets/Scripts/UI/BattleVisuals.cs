19a97961cb41faa7e70a3d21b13c07553653dabc
Kyle Cuss <108804713+Cussk@users.noreply.github.com>
2024-03-28 08:38:03 -0230

    ui and animations script

---
+++ Assets/Scripts/UI/BattleVisuals.cs
@@ -0,0 +1,71 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class BattleVisuals : MonoBehaviour
+    {
+        const string LVL_STRING = "Lvl: ";
+        const string IS_ATTACK_PARAM = "IsAttack";
+        const string IS_HIT_PARAM = "IsHit";
+        const string IS_DEAD_PARAM = "IsDead";
+        
+        [SerializeField] Slider healthBar;
+        [SerializeField] TextMeshProUGUI levelText;
+        Animator _animator;
+        int _currentHealth;
+        int _maxHealth;
+        int _level;
+
+        void Start()
+        {
+            _animator = gameObject.GetComponent<Animator>();
+            SetStartingValues(10, 10, 5);
+            PlayAttackAnimation();
+        }
+
+        void SetStartingValues(int currentHealth, int maxHealth, int level)
+        {
+            _currentHealth = currentHealth;
+            _maxHealth = maxHealth;
+            _level = level;
+            levelText.text = LVL_STRING + _level;
+            UpdateHealthBar();
+        }
+
+        public void ChangeHealth(int currentHealth)
+        {
+            _currentHealth = currentHealth;
+            
+            if (_currentHealth <= 0)
+            {
+                PlayDeathAnimation();
+                Destroy(gameObject);
+            }
+            
+            UpdateHealthBar();
+        }
+
+        void UpdateHealthBar()
+        {
+            healthBar.maxValue = _maxHealth;
+            healthBar.value = _currentHealth;
+        }
+
+        void PlayAttackAnimation()
+        {
+            _animator.SetTrigger(IS_ATTACK_PARAM);
+        }
+
+        void PlayHitAnimation()
+        {
+            _animator.SetTrigger(IS_HIT_PARAM);
+        }
+
+        void PlayDeathAnimation()
+        {
+            _animator.SetTrigger(IS_DEAD_PARAM);
+        }
+    }
+}
